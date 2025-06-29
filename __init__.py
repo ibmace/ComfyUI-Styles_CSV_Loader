3cc9eb73e745d215853d2d8c381f11d2a2751da9
ibmace <31149036+ibmace@users.noreply.github.com>
2025-06-29 21:28:31 +0530

    Fixed
    Corrected  styles_csv_loader.py logic and structure.
    
    Proper __init__.py with NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-from .styles_csv_loader import *
+
+from .styles_csv_loader import StylesCSVLoader
+
+NODE_CLASS_MAPPINGS = {
+    "Load Styles CSV": StylesCSVLoader
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Load Styles CSV": "Load Styles CSV"
+}
